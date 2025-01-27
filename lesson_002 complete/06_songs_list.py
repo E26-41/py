9e232a193529e48fdb7869c41631981bbde493a5
E26-41 <test@>
2025-01-27 18:53:22 +0500

    27012025
    Signed-off-by: E26-41 <test@>

--- lesson_002 complete/06_songs_list.py
+++ lesson_002 complete/06_songs_list.py
@@ -16,7 +16,7 @@ violator_songs_list = [
 ]
 
 # Распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
-#   Три песни звучат ХХХ минут
+# Три песни звучат ХХХ минут.
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
@@ -41,7 +41,7 @@ violator_songs_dict = {
 }
 
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
-#   А другие три песни звучат ХХХ минут
+# А другие три песни звучат ХХХ минут
 
 x1 = violator_songs_dict['Sweetest Perfection']
 y1 = violator_songs_dict['Policy of Truth']
