5febb292cb93c543f52d9a6068a782f67b8d5625
E36-073 <@test>
2025-02-08 20:26:39 +0500

    i dont know how to do this right now
    Signed-off-by: E36-073 <@test>

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -27,32 +27,36 @@ import simple_draw as sd
 # можно поиграть -шрифтами- цветами и углами отклонения
 
 # def draw_branches(start_point, angle, length):
-#     v = sd.get_vector(start_point=start_point, angle=angle + 30, length=length)
+#     v = sd.get_vector(start_point=start_point, angle=angle-30, length=length)
 #     v.draw()
-#     v = sd.get_vector(start_point=start_point, angle=angle - 30, length=length)
+#     v = sd.get_vector(start_point=start_point, angle=angle+30, length=length)
 #     v.draw()
-#
+
 # draw_branches(sd.get_point(250,225), 90, 50)
 
-def draw_branches_re(start_point, angle, length, delta):
-    if length <= 10:
-        return
+def draw_branche(start_point, angle, length):
     v = sd.get_vector(start_point=start_point, angle=angle, length=length)
     v.draw()
-    v1 = sd.get_vector(start_point=start_point, angle=angle+30, length=length)
-    v1.draw()
-    start_point = v.end_point
-    start_point1 = v1.end_point
-    angle = angle - delta
+    start_point1, start_point2 = v.end_point, v.end_point
+    angle1, angle2 = v.angle, v.angle
+    point = [start_point1, start_point2]
+    draw_branches(point, angle1, angle2, length)
+
+def draw_branches(point, angle1, angle2, length):
+    if length <= 10:
+        return
+    for start_point in point:
+        v1 = sd.get_vector(start_point=start_point, angle=angle1-30, length=length)
+        v1.draw()
+        v2 = sd.get_vector(start_point=start_point, angle=angle2-30, length=length)
+        v2.draw()
+        point += [v1.end_point, v2.end_point]
+        angle1, angle2 = v1.angle, v2.angle
     length = length * 0.75
-    draw_branches_re(start_point, angle, length, delta)
-    draw_branches_re(start_point1, angle, length, delta)
+    draw_branches(point, angle1, angle2, length)
 
+draw_branche(sd.get_point(300,100), angle=90, length=150)
 
-for delta in range(10, 20, 10):
-    draw_branches_re(sd.get_point(300,0), angle=90, length=150, delta=delta)
-for delta in range(-10, -9, 10):
-    draw_branches_re(sd.get_point(300,0), angle=90, length=150, delta=delta)
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
